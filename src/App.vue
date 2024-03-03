9d0d6bc4fc8b2cba37cfe97ef0c455eebc274e51
Bruno Tenório <tenoriobn@gmail.com>
2024-03-03 09:42:37 -0300

    Criando a primeira rota

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,7 @@
     </div>
 
     <div class="column is-three-quarter conteudo">
-      <Formulario @aoSalvarTarefa="salvarTarefa" />
-
-      <div class="lista">
-        <Tarefa 
-          v-for="(tarefa, index) in tarefas" 
-          :tarefa="tarefa" 
-          :key="index" 
-        />
-
-        <Box v-if="listaEstaVazia">
-          Você não está muito produtivo hoje :(
-        </Box>
-      </div>
+      <router-view></router-view>
     </div>
   </main>
 </template>
@@ -25,29 +13,16 @@
 <script lang="ts">
   import { defineComponent } from 'vue';
   import BarraLateral from './components/BarraLateral.vue';
-  import Formulario from './components/Formulario.vue';
-  import Tarefa from './components/Tarefa.vue';
-  import ITarefa from './interfaces/ITarefa'
-  import Box from './components/Box.vue';
 
   export default defineComponent({
       name: 'App',
-      components: { BarraLateral, Formulario, Tarefa, Box },
+      components: { BarraLateral },
       data() {
         return {
-          tarefas: [] as ITarefa[],
           modoEscuroAtivo: false
         }
       },
-      computed: {
-        listaEstaVazia() : boolean {
-          return this.tarefas.length === 0
-        }
-      },
       methods: {
-        salvarTarefa(tarefa: ITarefa) {
-          this.tarefas.push(tarefa)
-        },
         trocarTema(modoEscuroAtivo: boolean) {
           this.modoEscuroAtivo = modoEscuroAtivo
         }
