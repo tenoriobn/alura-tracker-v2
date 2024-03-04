2a4e07d35153c7a074b88ee8f924f3ec7d1262b0
Bruno Tenório <tenoriobn@gmail.com>
2024-03-04 12:25:13 -0300

    Modificando o estado e criando nova rota

--- src/components/BarraLateral.vue
+++ src/components/BarraLateral.vue
@@ -7,6 +7,23 @@
     <button class="button" @click="alterarTema">
       {{ textoBotao }}
     </button>
+
+    <nav class="panel mt-5">
+      <ul>
+        <li>
+          <router-link to="/" class="link">
+            <i class="fas fa-tasks"></i>
+            tarefas
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/projetos" class="link">
+            <i class="fas fa-project-diagram"></i>
+            projetos
+          </router-link>
+        </li>
+      </ul>
+    </nav>
   </header>
 </template>
 
@@ -47,6 +64,20 @@
     width: 100%;
     height: 100vh;
   }
+
+  .panel li {
+    margin: 8px 0;
+  }
+  .link {
+    color: #fff;
+  }
+  .link:hover {
+    color: #FAF0CA;
+  }
+  .link.router-link-active {
+    color: #FAF0CA;
+  }
+
   @media only screen and (max-width: 768px) {
     header {
       padding: 2.5rem;
