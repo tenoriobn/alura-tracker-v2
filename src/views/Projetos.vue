c6e5432f3213e1d8a6cb526c05d76548de226256
Bruno Tenório <tenoriobn@gmail.com>
2024-03-06 16:17:02 -0300

    Extraindo a view do

--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -2,32 +2,32 @@
   <section class="projetos">
     <h1 class="title">Projetos</h1>
 
-    <form @submit.prevent="salvar">
-      <div class="field">
-        <label for="nomeDoProjeto" class="label">
-          Nome do Projeto
-        </label>
-        <input type="text" class="input" v-model="nomeDoProjeto" id="nomeDoProjeto">
-      </div>
-
-      <div class="field">
-        <button class="button" type="submit">
-          Salvar
-        </button>
-      </div>
-    </form>
+    <router-link to="/projetos/novo" class="button">
+      <span class="icon is-small">
+        <i class="fas fa-plus"></i>
+      </span>
+      <span>Novo projeto</span>
+    </router-link>
 
     <table class="table is-fullwidth">
       <thead>
         <tr>
           <th>ID</th>
           <th>Nome</th>
+          <th>Ações</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="projeto in projetos" :key="projeto.id">
           <td>{{ projeto.id }}</td>
           <td>{{ projeto.nome }}</td>
+          <td>
+            <router-link :to="`/projetos/${projeto.id}`" class="button">
+              <span class="icon is-small">
+                <i class="fas fa-pencil-alt"></i>
+              </span>
+            </router-link>
+          </td>
         </tr>
       </tbody>
     </table>
@@ -40,23 +40,10 @@
 
   export default defineComponent({
     name: 'ProjetosComponent',
-    data() {
-      return {
-        nomeDoProjeto: '',
-      };
-    },
-    methods: {
-      salvar() {
-        this.store.commit('ADICIONA_PROJETO', this.nomeDoProjeto)
-
-        this.nomeDoProjeto = ''
-      }
-    },
     setup() {
       const store = useStore();
 
       return {
-        store,
         projetos: computed(() => store.state.projetos)
       }
     }
