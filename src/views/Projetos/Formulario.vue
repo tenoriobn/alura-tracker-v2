1a866ed6fcd9e6d0add952f96891fe12e5e0b2ae
Bruno Tenório <tenoriobn@gmail.com>
2024-03-23 12:13:03 -0300

    Notificando com mixins

--- src/views/Projetos/Formulario.vue
+++ src/views/Projetos/Formulario.vue
@@ -20,8 +20,9 @@
 <script lang="ts">
   import { defineComponent } from "vue";
   import { useStore } from "@/store"
-  import { ALTERA_PROJETO, ADICIONA_PROJETO, NOTIFICAR } from "@/store/tipo-mutacoes"
-import { TipoNotificacao } from "@/interfaces/INotificacao";
+  import { ALTERA_PROJETO, ADICIONA_PROJETO } from "@/store/tipo-mutacoes"
+  import { TipoNotificacao } from "@/interfaces/INotificacao";
+  import { notificacaoMixin } from '@/mixins/notificar';
 
   export default defineComponent({
     name: 'FormularioComponent',
@@ -30,6 +31,7 @@ import { TipoNotificacao } from "@/interfaces/INotificacao";
         type: String
       }
     },
+    mixins: [notificacaoMixin],
     mounted() {
       if(this.id) {
         const projeto = this.store.state.projetos.find(proj => proj.id === this.id)
@@ -53,13 +55,10 @@ import { TipoNotificacao } from "@/interfaces/INotificacao";
         }
 
         this.nomeDoProjeto = '';
-        this.store.commit(NOTIFICAR, {
-          titulo: 'Novo projeto foi salvo',
-          texto: 'Prontinho ;) seu projeto já está disponível.',
-          tipo: TipoNotificacao.SUCESSO
-        })
+        this.notificar(TipoNotificacao.SUCESSO, 'Excelente!', 'O projeto foi cadastrado com sucesso!');
         this.$router.push('/projetos');
-      }
+      },
+
     },
     setup() {
       const store = useStore();
